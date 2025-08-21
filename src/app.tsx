e1634f2b941037d2f10127a2e7c54aa6b99d516a
bogdanpreda97 <bobipreda@gmail.com>
2025-08-21 16:32:15 +0300

    add final sections

--- src/app.tsx
+++ src/app.tsx
@@ -4,10 +4,10 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/navbar.jsx";
 import Hero from "./components/hero.jsx";
 import Cocktails from "./components/cocktails.jsx";
-// import About from "./components/About.jsx";
-// import Art from "./components/Art.jsx";
-// import Menu from "./components/Menu.jsx";
-// import Contact from "./components/Contact.jsx";
+import About from "./components/about.jsx";
+import Art from "./components/art.jsx";
+import Menu from "./components/menu.jsx";
+import Contact from "./components/contact.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -17,6 +17,10 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
   );
 };
